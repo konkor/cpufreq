1f0cc8d4eb1ed6e878125e10a31eeb8ee640f411
konkor <kapa76@gmail.com>
2019-11-25 01:29:24 +0200

    Add thermal throttle and loadavg monitoring inside the service Update moving visual styling into the extension

--- extension.js
+++ extension.js
@@ -32,7 +32,10 @@ const MONITOR_KEY       = 'monitor';
 const EPROFILES_KEY     = 'event-profiles';
 const LABEL_KEY         = 'label'
 const LABEL_SHOW_KEY    = 'label-show';
-const UNITS_SHOW_KEY = 'units-show';
+const UNITS_SHOW_KEY    = 'units-show';
+const FREQ_SHOW_KEY     = 'frequency-show';
+const GOVS_SHOW_KEY     = 'governors-show';
+const LOAD_SHOW_KEY     = 'load-show';
 
 const COLOR_SHOW_KEY = 'color-show';
 const COLOR_SHOW_CUSTOM_KEY = 'color-show-custom';
@@ -62,6 +65,9 @@ let splash = true;
 let label_text = "";
 let label_show = false;
 let units_show = true;
+let frequency_show = true;
+let governor_show = false;
+let load_show = false;
 let title_text = "\u26A0";
 let title_style = "";
 let monitor_timeout = 500;
@@ -92,8 +98,8 @@ const OBJECT_PATH = '/org/konkor/cpufreq/service';
 const CpufreqServiceIface = '<node> \
 <interface name="org.konkor.cpufreq.service"> \
 <property name="Frequency" type="t" access="read"/> \
-<signal name="FrequencyChanged"> \
-  <arg name="title" type="s"/> \
+<signal name="MonitorEvent"> \
+  <arg name="metrics" type="s"/> \
 </signal> \
 <signal name="LoadingEvent"> \
   <arg name="loading" type="t"/> \
@@ -169,6 +175,9 @@ const FrequencyIndicator = new Lang.Class({
       label_text = o.get_string (LABEL_KEY);
       label_show = o.get_boolean (LABEL_SHOW_KEY);
       units_show = o.get_boolean (UNITS_SHOW_KEY);
+      frequency_show = o.get_boolean (FREQ_SHOW_KEY);
+      governor_show = o.get_boolean (GOVS_SHOW_KEY);
+      load_show = o.get_boolean (LOAD_SHOW_KEY);
       color_show = o.get_boolean (COLOR_SHOW_KEY);
       color_show_custom = o.get_boolean (COLOR_SHOW_CUSTOM_KEY);
       color_show_custom_normal = o.get_string (COLOR_SHOW_CUSTOM_NORMAL_KEY);
@@ -197,9 +206,25 @@ const FrequencyIndicator = new Lang.Class({
     } else if (key == LABEL_KEY) {
       label_text = o.get_string (LABEL_KEY);
     } else if (key == LABEL_SHOW_KEY) {
-      label_show = o.get_string (LABEL_SHOW_KEY);
+      label_show = o.get_boolean (LABEL_SHOW_KEY);
     } else if (key == UNITS_SHOW_KEY) {
-      units_show = o.get_string (UNITS_SHOW_KEY);
+      units_show = o.get_boolean (UNITS_SHOW_KEY);
+    } else if (key == FREQ_SHOW_KEY) {
+      frequency_show = o.get_boolean (FREQ_SHOW_KEY);
+    } else if (key == GOVS_SHOW_KEY) {
+      governor_show = o.get_boolean (GOVS_SHOW_KEY);
+    } else if (key == LOAD_SHOW_KEY) {
+      load_show = o.get_boolean (LOAD_SHOW_KEY);
+    }else if (key == COLOR_SHOW_KEY) {
+      color_show = o.get_boolean (COLOR_SHOW_KEY);
+    } else if (key == COLOR_SHOW_CUSTOM_KEY) {
+      color_show_custom = o.get_boolean (COLOR_SHOW_CUSTOM_KEY);
+    } else if (key == COLOR_SHOW_CUSTOM_NORMAL_KEY) {
+      color_show_custom_normal = o.get_string (COLOR_SHOW_CUSTOM_NORMAL_KEY);
+    } else if (key == COLOR_SHOW_CUSTOM_WARNING_KEY) {
+      color_show_custom_warning = o.get_string (COLOR_SHOW_CUSTOM_WARNING_KEY);
+    } else if (key == COLOR_SHOW_CUSTOM_CRITICAL_KEY) {
+      color_show_custom_critical = o.get_string (COLOR_SHOW_CUSTOM_CRITICAL_KEY);
     }
 
     //TODO: statusLabel
@@ -267,9 +292,84 @@ const FrequencyIndicator = new Lang.Class({
   },
 
   get_title: function (text) {
-    text = text || label_text;
-    title_text = text.trim ();
+    if (!text) return title_text;
+    let metrics = JSON.parse (text), s = "", f = 0, units;
+    if (frequency_show) {
+      f = metrics.frequency_maximum;
+      if (f) if (f < 1000000) {
+        units = " \u3392";
+        s = (f / 1000).toFixed(0).toString ();
+      } else {
+        units = "\u3393";
+        s = (f / 1000000).toFixed(2).toString ();
+      }
+      if (units_show) s += units;
+    }
+    if (governor_show && metrics.governor) {
+      s += " " + this.get_governor_symbolyc (metrics.governor);
+    }
+    if (load_show && Number.isInteger (metrics.state))
+      s += ' ' + this.get_state_symbolyc (metrics.state);
+    if (label_show) s += ' ' + label_text;
+    if (s) title_text = s.trim ();
+    else title_text = label_text;
+
+    if (color_show && Number.isInteger (metrics.state)) {
+      s = this.get_stylestring (metrics.state);
+    } else title_style = "";
+    if (s != title_style) {
+      title_style = s;
+      this.statusLabel.style = title_style;
+    }
     return title_text;
+  },
+
+  get_governor_symbolyc: function (name) {
+    let g = name;
+    if (g == "mixed") g = "\u25cd";
+    else if (g == "powersave") g = "\uf06c";
+    else if (g == "performance") g = "\uf197";
+    else if (g == "ondemand") g = "\uf0e7";
+    else if (g == "conservative") g = "\ue976";
+    else if (g == "schedutil") g = "\ue953";
+    else if (g == "userspace") g = "\uf007";
+    else g = "\uf0e7";
+    return g;
+  },
+
+  get_state_symbolyc: function (state) {
+    let g = "☺";
+    if (state == 1) g = "";
+    else if (state == 2) g = "☹";
+    return g;
+  },
+
+  get_stylestring: function (state) {
+    let s;
+    if (color_show_custom) state += 3;
+    switch (state) {
+      case 0:
+        s = "color:" + color_show_default_normal + ";";
+        break;
+      case 1:
+        s = "color:" + color_show_default_warning + ";";
+        break;
+      case 2:
+        s = "color:" + color_show_default_critical + ";";
+        break;
+      case 3:
+        s = "color:" + color_show_custom_normal + ";";
+        break;
+      case 4:
+        s = "color:" + color_show_custom_warning + ";";
+        break;
+      case 5:
+        s = "color:" + color_show_custom_critical + ";";
+        break;
+      default:
+        s = "";
+    }
+    return s;
   },
 
   add_event: function () {
@@ -284,8 +384,8 @@ const FrequencyIndicator = new Lang.Class({
           error (e.message);
           return;
         }
-        event = this.proxy.connectSignal ('FrequencyChanged', (o, s, title) => {
-          if (title) this.statusLabel.set_text (this.get_title (title.toString ()));
+        event = this.proxy.connectSignal ('MonitorEvent', (o, s, metrics) => {
+          if (metrics) this.statusLabel.set_text (this.get_title (metrics.toString ()));
         });
         event_style = this.proxy.connectSignal ('StyleChanged', (o, s, style) => {
           if (style) {
